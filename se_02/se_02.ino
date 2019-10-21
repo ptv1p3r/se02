646f16a6c933801d3af3d24df3d7051abed3186b
Pedro Roldan <pedro.roldan@gmail.com>
2019-10-21 12:54:56 +0100

    .

--- se_02/se_02.ino
+++ se_02/se_02.ino
@@ -7,7 +7,9 @@
 #define DHTPIN 2          //pin DHT sensor 
 #define DHTTYPE DHT22     //tipo de sensor
 #define OLED_ADDR   0x3C  // endereço i2c oled
-#define LDRPIN A0         //pin DHT sensor 
+#define LDRPIN A0         //pin DHT sensor
+#define FC37PINA A2       //pin FC37 analogico sensor
+#define FC37PIND 7        //pin FC37 digital sensor 
 
 DHT dht(DHTPIN, DHTTYPE); //instacia sensor
 
@@ -24,6 +26,9 @@ float humidityValue = 0;    // humidade
 float temperatureValue = 0; // temperatura
 float heatIndexValue = 0;   // heat index
 int lightValue = 0;         // light value
+int rainMax = 1024;         // rain max value
+int rainMin = 0;            // rain min value
+int rainValue = 0;          // rain value
 
 void setup() {
   // initialize and clear display
@@ -32,16 +37,17 @@ void setup() {
 
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(27,30);
-  display.print("Hello, world!");
+  display.setCursor(20,32);
+  display.print("Lendo dados...");
   
   display.display();
   
   Serial.begin(9600);
-  Serial.println(F("teste leituras:"));
+
+  pinMode(FC37PIND, INPUT);
 
   dht.begin();
-
+  Serial.println(F("teste leituras:"));
 }
 
 void loop() {
@@ -50,6 +56,7 @@ void loop() {
   humidityValue = dht.readHumidity(); // valor humidade em celcius
   temperatureValue = dht.readTemperature(); // valor temperatura celcius 
   lightValue = analogRead(LDRPIN);
+  rainValue = analogRead(FC37PINA);
 
   // valida valores de sensores.
   if (isnan(humidityValue) || isnan(temperatureValue)) {
@@ -59,6 +66,8 @@ void loop() {
   // calcula heat index em celcius
   heatIndexValue = dht.computeHeatIndex(temperatureValue, humidityValue, false);
 
+  ScreenUpdate(temperatureValue, humidityValue, heatIndexValue, lightValue, rainValue);
+  
   Serial.print(F("Humidity: "));
   Serial.print(humidityValue);
   Serial.print(F("%  Temperature: "));
@@ -68,5 +77,46 @@ void loop() {
   Serial.print(heatIndexValue);
   Serial.print(F("°C "));
   Serial.print(lightValue);
-  Serial.println(F("ldr "));
+  Serial.print(F("ldr "));
+  Serial.print(rainValue);
+  Serial.println(F(" rain "));
+}
+
+void ScreenUpdate(float tempValue,float humValue, float heatIndex, int luxValue, float precipValue){
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  // temperatura
+  display.setCursor(0,16);
+  display.print("Temp:");
+  display.setCursor(30,16);
+  display.print(tempValue);
+
+  // heat index
+  display.setCursor(0,25);
+  display.print("HI:");
+  display.setCursor(18,25);
+  display.print(heatIndex);
+
+  // humidade
+  display.setCursor(0,35);
+  display.print("Humidade:");
+  display.setCursor(53,35);
+  display.print(humValue);
+
+  // lux
+  display.setCursor(0,45);
+  display.print("Lux:");
+  display.setCursor(23,45);
+  display.print(luxValue);
+
+  // precipitacao
+  display.setCursor(0,55);
+  display.print("Chuva:");
+  display.setCursor(35,55);
+  display.print(precipValue);
+  
+  display.display();
 }
